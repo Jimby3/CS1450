5582421f024c964d9428d09a357253a74cd66780
Jimby3 <jimever333@gmail.com>
2023-01-25 19:41:10 -0700

    finished assignment 1

--- CS1140/OHaraJimmyAssignment1.java
+++ CS1140/OHaraJimmyAssignment1.java
@@ -1,3 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
 /* Name: Jimmy O'Hara
  * Class: CS1450 (T/TH) 
  * Due: 1/26/23
@@ -8,7 +15,7 @@
 
 public class OHaraJimmyAssignment1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// declaring constants
 		final int MINIMUM = 10;
@@ -21,35 +28,106 @@ public class OHaraJimmyAssignment1 {
 		// putting random numbers into array
 		for (int i = 0; i < randArray.length; i++) {
 			randArray[i] = MINIMUM + (int) (Math.random() * MAXIMUM);
+			System.out.println(randArray[i]);
 		}
 
-		// displaying the array
-		printArray(randArray);
 
 		// task #2 | Sum, Average, and Median, and Largest Num
 
 		// Sum
 		int sum = sum(randArray);
-		System.out.println(sum);
+		System.out.println("\n\nSum: " + sum);
 
 		// Average
 		double average = average(sum, randArray.length);
-		System.out.println(average);
+		System.out.println("Average: " + average);
 
 		// Median
-		sortArray(randArray);
+		Arrays.sort(randArray);
+		
+		int medianIndex = median(randArray);
+		
+		System.out.println("Median: " + randArray[medianIndex]);
 
-		printArray(randArray);
-
-		for (int i = 0; i < randArray.length; i++) {
-
-		}
-
+		
 		// Largest Value
+		int largestValue = largestValue(randArray);
+		int largestCounter = largestFrequency(randArray, largestValue);
+		
+		System.out.println("Largest Value: " + largestValue + " which displayed " + largestCounter + " time(s)");
+		
+		
+		//Task #3 | 2D array
+		
+		//Declaring Constants
+		final int ROWS = 4;
+		final int COLUMNS = 5;
+		
+		
+		//Creating the 2D array
+		int[][] array2D = new int[ROWS][COLUMNS];
+		
+		
+		//Counter for orignal array
+		int randArrayIndex = 0;
+		
+		//row for loop
+		for (int i = 0; i < ROWS ; i++) {
+			
+			System.out.println("\n");
+			
+			//column for loop
+			for (int j = 0; j < COLUMNS; j++) {
+				
+				array2D[i][j] = randArray[randArrayIndex];
+				
+				randArrayIndex++;
+				
+				System.out.print(array2D[i][j] + " ");
+				
+			}//column
+			
+		}//row
+		
+		
+		//printing original array but sorted
+		System.out.print("\n\n\n");
 		for (int i = 0; i < randArray.length; i++) {
-
+			System.out.println(randArray[i]);
 		}
 
+		//Task #4 | Printing to a file
+		
+		File fileName = new File("assignment1.txt");
+		
+		PrintWriter outputFile = new PrintWriter(fileName);
+		
+		//counter for randArray
+		randArrayIndex = 0;
+		
+		//row for loop
+		for (int i = 0; i < ROWS ; i++) {
+			
+			outputFile.print("\n");
+			
+			//column for loop
+			for (int j = 0; j < COLUMNS; j++) {
+				
+				array2D[i][j] = randArray[randArrayIndex];
+				
+				randArrayIndex++;
+				
+				outputFile.print(array2D[i][j] + " ");
+
+			}//column
+			
+		}//row
+		
+		outputFile.close();
+		
+		System.out.println("\n\nFile is in directory: " + fileName.getAbsolutePath());
+		
+		
 	}// main
 
 	public static void printArray(int[] array) {
@@ -82,15 +160,37 @@ public class OHaraJimmyAssignment1 {
 	}// average
 
 	// method for finding the median in an array
+	public static int median(int[] array) { // figure out what to put here
+		
+		double medianIndex = 0;
+		
+		medianIndex = (array.length / 2) - 1;
+		int indexReal = (int)Math.round(medianIndex);
 
-	public static void sortArray(int[] array) {
-		for (int i = 0; i < array.length; i++) {
-			
-		}
+		return indexReal;
+	}//median
+	
+	//Finding largest number in largest array
+	public static int largestValue(int[] sortedArray) {
+		
+		int largestIndex = sortedArray.length -1;
+		int largest = sortedArray[largestIndex];
+		
+		return largest;
 	}
-
-	public static int median() { // figure out what to put here
-		// changes again again
+	
+	//Finding how many times the largest number happens in an array
+	public static int largestFrequency(int[] array, int largest) {
+		
+		int frequency = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == largest) {
+				frequency++;
+			}
+		}
+		
+		return frequency;
 	}
 
 }// Assignment 1
